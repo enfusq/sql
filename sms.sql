f4bc756c1beddd81a41e85dd261fcde0dd302efd
Aivars <example@email.com>
2024-10-28 10:09:16 +0200

    before pd

---
+++ sms.sql
@@ -0,0 +1,33 @@
+-- USE school_management_system;
+
+-- CREATE TABLE profile (
+-- 	profile_id int NOT NULL auto_increment,
+--     student_id int NOT NULL,
+--     address varchar(45),
+--     mobile varchar(45),
+--     PRIMARY KEY(profile_id),
+--     FOREIGN KEY (student_id) REFERENCES students(student_id)
+-- );
+
+-- CREATE TABLE students (
+-- 	student_id int NOT NULL auto_increment,
+--     student_name varchar(45),
+--     student_email varchar(45),
+--     PRIMARY KEY(student_id)
+-- );
+
+-- CREATE TABLE teachers (
+-- 	teacher_id int NOT NULL auto_increment,
+--     PRIMARY KEY(teacher_id)
+-- );
+-- CREATE TABLE courses (
+-- 	course_id int NOT NULL auto_increment,
+--     PRIMARY KEY(course_id)
+-- );
+-- CREATE TABLE departments (
+-- 	department_id int NOT NULL auto_increment,
+--     PRIMARY KEY(department_id)
+-- );
+
+
+
